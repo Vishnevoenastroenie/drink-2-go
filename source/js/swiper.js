e302ac0b77a4807ba1e239d96ac1e696f302a562
Наталья Лягина <Vischnevoenastroenie@yandex.ru>
2023-02-20 01:12:56 +0400

    Добавляет разметку по БЭМ, слайдер и часть стилей

---
+++ source/js/swiper.js
@@ -0,0 +1,12 @@
+document.addEventListener('DOMContentLoaded', () => {
+  new Swiper('.swiper', {
+    loop: true,
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  })
+})
